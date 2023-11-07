cbf86d0560afbd602c897008636908c117f658e6
sbx_user1051 <sbx_user1051@169.254.37.61>
2023-11-07 20:12:19 +0000

    Tue Nov 07 20:12:19 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,14 +7,47 @@ import java.util.Objects;
 
 public class CheckoutSolution {
 
+    record SpecialOffer(int quantity, int price) {
+    }
 
-    private Map<String, Objects> store = new HashMap<>(){{
+    static class ItemProcessed {
+        private int price;
+        private int quantity = 0;
+        private SpecialOffer specialOffer = null;
+
+        public ItemProcessed(int price) {
+            this.price = price;
+        }
+
+        public ItemProcessed(int price, SpecialOffer specialOffer) {
+            this.price = price;
+            this.specialOffer = specialOffer;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public SpecialOffer getSpecialOffer() {
+            return specialOffer;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity; //todo
+        }
+    }
+
+
+    private Map<String, Objects> store = new HashMap<>() {{
         put("A", );
         put("B", );
         put("C", );
         put("D", );
     }};
-
 
 
     public Integer checkout(String skus) {
@@ -36,6 +69,7 @@ public class CheckoutSolution {
         //Arrays.stream("ABC".split("")).peek(a -> System.out.println(a)).toList();
     }*/
 }
+
 
 
 
