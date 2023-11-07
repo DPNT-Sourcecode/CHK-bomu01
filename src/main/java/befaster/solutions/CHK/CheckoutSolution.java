261776a9f6f61848f63ea3a3835cf35c72b5ab11
sbx_user1051 <sbx_user1051@169.254.37.61>
2023-11-07 20:06:19 +0000

    Tue Nov 07 20:06:19 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,9 +1,35 @@
 package befaster.solutions.CHK;
 
-import befaster.runner.SolutionNotImplementedException;
+import java.util.Arrays;
 
 public class CheckoutSolution {
+
+
+
+
     public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
+
+        if (skus == null) {
+            return -1;
+        } else if (skus.equals("")) { //check this
+            return 0;
+        }
+
+
+
+
+        return 0; //TODO
+    }
+
+    public static void main(String[] args) {
+        Arrays.stream("ABC".split("")).peek(a -> System.out.println(a)).toList();
     }
 }
+
+
+
+
+
+
+
+
